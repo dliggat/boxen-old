496a8cd818a9f5ad01e7559b24b2c2cb75adcdad
Dave Liggat <liggsie@gmail.com>
2013-07-04 15:26:42 -0400

    Adding vimrc files

--- modules/people/manifests/liggsie/config/dotfiles.pp
+++ modules/people/manifests/liggsie/config/dotfiles.pp
@@ -22,61 +22,17 @@ class people::liggsie::config::dotfiles (
       path     => "${my_sourcedir}/dotfiles";
   }
 
-  
+  file { "${my_homedir}/.gvimrc":
+    ensure  => link,
+    target  => "${my_sourcedir}/dotfiles/gvimrc",
+    require => Repository["dotfiles"],
+  }
 
-  # file { "${my_homedir}/.gvimrc":
-  #   ensure  => link,
-  #   target  => "${my_sourcedir}/dotfiles/gvimrc",
-  #   require => Repository["dotfiles"],
-  # }
-
-  # file { "${my_homedir}/.login_conf":
-  #   ensure  => link,
-  #   target  => "${my_sourcedir}/dotfiles/login_conf",
-  #   require => Repository["dotfiles"],
-  # }
-
-  # file { "${my_homedir}/.tmux.conf":
-  #   ensure  => link,
-  #   target  => "${my_sourcedir}/dotfiles/tmux.conf",
-  #   require => Repository["dotfiles"],
-  # }
-
-  # file { "${my_homedir}/.slate.js":
-  #   ensure  => link,
-  #   target  => "${my_sourcedir}/dotfiles/slate.js",
-  #   require => Repository["dotfiles"],
-  # }
-
-  # file { "${my_homedir}/.vimrc":
-  #   ensure  => link,
-  #   target  => "${my_sourcedir}/dotfiles/vimrc",
-  #   require => Repository["dotfiles"],
-  # }
-
-  # file { "${my_homedir}/.zlogin":
-  #   ensure  => link,
-  #   target  => "${my_sourcedir}/dotfiles/zlogin",
-  #   require => Repository["dotfiles"],
-  # }
-
-  # file { "${my_homedir}/.zprezto":
-  #   ensure  => link,
-  #   target  => "${my_sourcedir}/dotfiles/prezto",
-  #   require => Repository["dotfiles"],
-  # }
-
-  # file { "${my_homedir}/.zpreztorc":
-  #   ensure  => link,
-  #   target  => "${my_sourcedir}/dotfiles/zpreztorc",
-  #   require => Repository["dotfiles"],
-  # }
-
-  # file { "${my_homedir}/.zshenv":
-  #   ensure  => link,
-  #   target  => "${my_sourcedir}/dotfiles/zshenv",
-  #   require => Repository["dotfiles"],
-  # }
+  file { "${my_homedir}/.vimrc":
+    ensure  => link,
+    target  => "${my_sourcedir}/dotfiles/vimrc",
+    require => Repository["dotfiles"],
+  }
 
   file { "${my_homedir}/.bash_profile":
     ensure  => link,
@@ -89,11 +45,5 @@ class people::liggsie::config::dotfiles (
     target  => "${my_sourcedir}/dotfiles/bashrc",
     require => Repository["dotfiles"],
   }
-
-  # file { "${my_homedir}/Library/Preferences/com.googlecode.iterm2.plist":
-  #   ensure  => link,
-  #   target  => "${my_sourcedir}/dotfiles/osx/com.googlecode.iterm2.plist",
-  #   require => Repository["dotfiles"],
-  # }
 
 }
