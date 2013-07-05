505fa9f54f01a22c58cfed38ba471e2b58e01c9e
Dave Liggat <liggsie@gmail.com>
2013-07-05 14:41:48 -0400

    Added vmware and sequelpro

--- modules/people/manifests/liggsie/applications/main.pp
+++ modules/people/manifests/liggsie/applications/main.pp
@@ -11,12 +11,13 @@ class people::liggsie::applications::main {
   include iterm2::stable
   include onepassword
   include redis
+  include sequel_pro
   include skype
   include textwrangler
   include transmission
   include vim
   include vlc
+  include vmware_fusion
   include wget
   include xquartz
-
 }
