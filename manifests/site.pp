55b9495294c1056f1964c1ac190f3d193248f713
Dave Liggat <dliggat@gmail.com>
2013-07-05 11:59:31 -0400

    Setting default ruby to be 1.9.3

--- manifests/site.pp
+++ manifests/site.pp
@@ -75,10 +75,11 @@ node default {
   include nodejs::v0_8
   include nodejs::v0_10
 
-  # default ruby versions
+  # Ruby versions
+  class { 'ruby::global':
+    version => '1.9.3'
+  }
   include ruby::1_8_7
-  include ruby::1_9_2
-  include ruby::1_9_3
   include ruby::2_0_0
 
   # common, useful packages
